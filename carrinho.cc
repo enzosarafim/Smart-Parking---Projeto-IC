69953f2da911e977dfc41175427616b4f7e18b11
Enzo Nogueira Sarafim <enzosarafim@gmail.com>
2025-12-19 10:28:42 -0300

    Integrate ultrasonic sensors and LCD for parking system
    Refactor car parking system to include ultrasonic sensors and LCD display.

--- carrinho.cc
+++ carrinho.cc
@@ -1,84 +1,107 @@
-# - Carrinho Bluetooth -
+#include <LiquidCrystal_I2C.h>
 
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
-#include <Dabble.h>
-  
-#define INT1 3
-#define INT2 4
-#define INT3 5
-#define INT4 6
-  
-void setup()
-{
-  Serial.begin(250000);
-  Dabble.begin(9600,11,10);  
-  pinMode(INT1,OUTPUT);
-  pinMode(INT2,OUTPUT);
-  pinMode(INT3,OUTPUT);                                                                    
-  pinMode(INT4,OUTPUT);
-  Stop();
+// CONFIGURAÇÃO DOS PINOS
+#define trig1 4
+#define echo1 5
+#define trig2 9
+#define echo2 10
+
+int led1Verm = 3;
+int led1Verde = 2;
+int led2Verm = 11;
+int led2Verde = 12;
+
+//VARIÁVEIS 
+int sensor1 = 0;
+int sensor2 = 0;
+int vagasocupadas = 0;
+int vagaslivres = 2;
+
+// LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// FUNÇÃO PARA LER SENSOR ULTRASSÔNICO
+long readUltrasonicDistance(int triggerPin, int echoPin) {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration / 58; // Converte tempo em cm
+
+  if (distance == 0) return 9999; // checar se está funcionando
+  return distance;
 }
-  
-  
+
+void setup() {
+  Serial.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+
+  pinMode(trig1, OUTPUT);
+  pinMode(echo1, INPUT);
+  pinMode(trig2, OUTPUT);
+  pinMode(echo2, INPUT);
+
+  pinMode(led1Verm, OUTPUT);
+  pinMode(led1Verde, OUTPUT);
+  pinMode(led2Verm, OUTPUT);
+  pinMode(led2Verde, OUTPUT);
+}
+
 void loop() {
-  
-  
-Dabble.processInput();          
-  
-if(GamePad.isUpPressed())
-{
- forward(); 
-}
-else if(GamePad.isDownPressed())
-{
- backward(); 
-}
-else if(GamePad.isLeftPressed())
-{
- left(); 
-}
-else if(GamePad.isRightPressed())
-{
- right(); 
-}
-else
-{
- Stop(); 
-}
-  
-}
-void forward() {
-  digitalWrite(INT1,LOW);
-  digitalWrite(INT2,HIGH);
-  digitalWrite(INT3,HIGH);
-  digitalWrite(INT4,LOW);
-}
-     
-void backward() {
-  digitalWrite(INT1,HIGH);
-  digitalWrite(INT2,LOW);
-  digitalWrite(INT3,LOW);
-  digitalWrite(INT4,HIGH);
-}
-     
-void left() {
-  digitalWrite(INT1,HIGH);
-  digitalWrite(INT2,LOW);
-  digitalWrite(INT3,HIGH);
-  digitalWrite(INT4,LOW);    
-}
-     
-void right() {
-  digitalWrite(INT1,LOW);
-  digitalWrite(INT2,HIGH);
-  digitalWrite(INT3,LOW);
-  digitalWrite(INT4,HIGH);  
-}
-     
-void Stop() {
-  digitalWrite(INT1,LOW);
-  digitalWrite(INT2,LOW);
-  digitalWrite(INT3,LOW);
-  digitalWrite(INT4,LOW);  
+
+  // LEITURA DOS SENSORES
+  long dist1 = readUltrasonicDistance(trig1, echo1);
+  long dist2 = readUltrasonicDistance(trig2, echo2);
+
+  Serial.print("Sensor 1: ");
+  Serial.print(dist1);
+  Serial.println(" cm");
+
+  Serial.print("Sensor 2: ");
+  Serial.print(dist2);
+  Serial.println(" cm");
+
+  // SENSOR 1
+  if (dist1 > 1 && dist1 < 50) {
+    digitalWrite(led1Verm, HIGH);
+    digitalWrite(led1Verde, LOW);
+    sensor1 = 1;
+  } else {
+    digitalWrite(led1Verm, LOW);
+    digitalWrite(led1Verde, HIGH);
+    sensor1 = 0;
+  }
+
+  // SENSOR 2 
+  if (dist2 > 1 && dist2 < 50) {
+    digitalWrite(led2Verm, HIGH);
+    digitalWrite(led2Verde, LOW);
+    sensor2 = 1;
+  } else {
+    digitalWrite(led2Verm, LOW);
+    digitalWrite(led2Verde, HIGH);
+    sensor2 = 0;
+  }
+
+  // CONTAGEM DAS VAGAS
+  vagasocupadas = sensor1 + sensor2;
+  vagaslivres = 2 - vagasocupadas;
+
+  // LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Vagas livres = ");
+  lcd.print(vagaslivres);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Vagas ocup. = ");
+  lcd.print(vagasocupadas);
+
+  delay(200);
 }
